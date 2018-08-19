135ce8175ea826de4342081578acf7a62d87c939
f <f@fde-MacBook-Pro.local>
2018-08-19 20:13:11 +0800

    Modify commit script

--- commit.sh
+++ commit.sh
@@ -1,9 +1,29 @@
 #! /bin/bash
 mv Linear\ Algebra.zip Linear\ Algebra.tmp
-echo "Enter Zip Password :"
+
+echo "Enter Zip Password"
 zip -er Linear\ Algebra.zip Linear\ Algebra
+
+if [[ $? -ne 0 ]];then
+    mv Linear\ Algebra.tmp Linear\ Algebra.zip; exit 1
+fi
+
 echo "Finish Zip !"
+
 git add .
 git commit
+
+if [[ $? -ne 0 ]];then
+    rm Linear\ Algebra.zip;
+    mv Linear\ Algebra.tmp Linear\ Algebra.zip;
+    exit 1;
+fi
+
 git push
+if [[ $? -ne 0 ]];then
+    rm Linear\ Algebra.zip;
+    mv Linear\ Algebra.tmp Linear\ Algebra.zip;
+    exit 1;
+fi
+
 rm -i Linear\ Algebra.tmp
